26f4739fb92e484a5a23d0b97e0d060999f3f751
Ben Southgate <bsouthga@gmail.com>
2015-01-20 18:28:22 -0500

    first commit

---
+++ explode.js
@@ -0,0 +1,100 @@
+/*
+  D3 Explode! extension
+  Ben Southgate
+
+  Copyright (c) 2015 Benjamin Southgate
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+;(function(d3) {
+
+d3.geo.explode = function explode() {
+
+  function exploder(selection) {
+    // check for necessary parameters
+    ["size", "align", "projection"].forEach(function(option) {
+      if (!config[option]) {
+        throw "explode.js: " + option + " not provided to explode.";
+      }
+    });
+    // local references to configuration
+    var projection = config.projection,
+        size = config.size,
+        align = config.align,
+        scale = projection.scale(),
+        path = d3.geo.path().projection(projection),
+        cache = {};
+    // move features based on configuration
+    selection
+      // the order of the attribute changes matters!
+      // setting 'd' caches the new scale which is used
+      // in the transform change
+      .attr('d', function(d, i) {
+        // compute size and alignment based on user functions
+        var sz = size(d, i);
+        // calculate new scale for projection based on desired
+        // pixed size of feature
+        var bounds = path.projection(projection.scale(scale))
+                         .bounds(d.feature),
+            w = bounds[1][0] - bounds[0][0],
+            h = bounds[1][1] - bounds[0][1],
+            sc = cache[i] = Math.min(sz / h, sz / w);
+        // scale projection to desired size
+        // and calculate new centroid to translate to
+        path.projection(projection.scale(scale*sc));
+        // return scaled path
+        return path(d.feature);
+      })
+      // calculate new transform after finding scale
+      .attr("transform", function(d, i) {
+        // compute size and alignment based on user functions
+        var A = align(d, i),
+            δx = A[0],
+            δy = A[1];
+        // scale projection to desired size
+        // and calculate new centroid to translate to
+        path.projection(projection.scale(scale*cache[i]));
+        var coords = path.centroid(d.feature),
+            cx = coords[0],
+            cy = coords[1];
+        // return desired coordinates offset by centroid
+        return "translate(" + [δx - cx, δy - cy] + ")";
+    });
+    // reset scale for projection
+    projection.scale(scale);
+  }
+
+  // create getters / setters
+  var config = {};
+  ["size", "align", "projection"].forEach(function(option) {
+    config[option] = null;
+    exploder[option] = function(value) {
+      if (!arguments.length) return config[option];
+      config[option] = value;
+      return exploder;
+    };
+  });
+
+  exploder.version = "1.0.0";
+  // return configurable exploder function
+  return exploder;
+};
+
+})(d3);
